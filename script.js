8c62c28fadb61cca57d294aa6ab103002b361535
Miguel Ángel Pérez <angeldev1@outlook.com>
2025-04-29 16:17:54 -0600

    feat: prototipo del fomulario para un test vocacional - UIA

---
+++ script.js
@@ -0,0 +1,147 @@
+const questions = [
+  {
+    question: "¿Qué te gusta hacer en las vacaciones?",
+    step: 1,
+    answers: [
+      "Jugar con niños pequeños/sobrinos",
+      "Organizar las vacaciones",
+      "Crear contenido para redes sociales",
+      "Aprovechar el tiempo para dibujar",
+      "Buscar una fuente de ingresos",
+      "Preparar postres",
+      "Ponerme a dieta / hacer ejercicio",
+      "Jugar en el celular / consola",
+      "Ver documentales / leer libros"
+    ]
+  },
+  {
+    question: "Si te dieran la oportunidad de entrar a un trabajo con plaza, ¿cuál escogerías?",
+    step: 2,
+    answers: [
+      "Dar clases en una escuela primaria / kínder",
+      "Trabajar en una agencia de viajes",
+      "Conductor en un programa de TV para Telemundo/Telesisa",
+      "Diseñador gráfico en una agencia de marketing internacional",
+      "Ser CEO de una empresa de marketing digital",
+      "Chef de cruceros",
+      "Organización enfocada en educación de comida saludable",
+      "Empresa que desarrolle inteligencia artificial y robótica",
+      "Trabajar en una firma de abogados / despacho jurídico"
+    ]
+  },
+  {
+    question: "Si tuvieras la oportunidad de poner tu propio negocio, ¿de qué sería?",
+    step: 3,
+    answers: [
+      "Centro educativo para regularización primaria/secundaria/bachillerato",
+      "Empresa que ofrezca tours a sitios turísticos locales",
+      "Agencia de contenido audiovisual",
+      "Estudio de diseño creativo que trabaje con Pixar/Dreamworks",
+      "Agencia de publicidad",
+      "Empresa de catering de alimentos y bebidas",
+      "Franquicia de ensaladas / comida vegana",
+      "Empresa de energías renovables / IA / videojuegos",
+      "Despacho de asesoría jurídica"
+    ]
+  },
+  {
+    question: "¿Qué prefieres hacer en tu tiempo libre?",
+    step: 4,
+    answers: [
+      "Hacer manualidades",
+      "Visitar lugares turísticos locales",
+      "Grabar TikToks",
+      "Ver videos para aprender dibujo/pintura",
+      "Crear tendencias en redes sociales",
+      "Tomar clases / cursos de cocina",
+      "Ir al gimnasio",
+      "Jugar videojuegos / experimentar con ChatGPT",
+      "Ver Caso Cerrado"
+    ]
+  },
+  {
+    question: "¿En qué te consideras mejor?",
+    step: 5,
+    answers: [
+      "Enseñar",
+      "Socializar",
+      "Comunicarme con las personas",
+      "Dibujar / Pintar",
+      "Saber negociar",
+      "Preparando postres",
+      "Seguir una dieta",
+      "Programar",
+      "Resolver problemas o conflictos"
+    ]
+  },
+  {
+    question: "¿Qué actividades se te facilitan más?",
+    step: 6,
+    answers: [
+      "Compartir / transmitir conocimiento",
+      "Organizar eventos",
+      "Grabar y editar videos",
+      "Utilizar Photoshop / Illustrator",
+      "Ventas",
+      "Realizar las compras en el súper",
+      "Hacer ejercicio",
+      "Diseñar / crear ideas nuevas de videojuegos",
+      "Participar en debates"
+    ]
+  },
+  {
+    question: "¿Qué consideras que tiene más relevancia para tu futuro?",
+    step: 7,
+    answers: [
+      "Ser director de mi propia institución",
+      "Dominar varios idiomas (políglota)",
+      "Ganar un Pulitzer / Oscar",
+      "Ser un diseñador gráfico famoso",
+      "Ser publicista de la marca Dior",
+      "Ser un chef reconocido de comida internacional",
+      "Tener una franquicia de consultorios nutricionales",
+      "Trabajar para Google / Facebook",
+      "Llegar a ser un juez"
+    ]
+  }
+];
+
+
+const carreers = [
+  {
+    carreer: "Pedagogía",
+    index: "a"
+  },
+  {
+    carreer: "Turismo",
+    index: "b"
+  },
+  {
+    carreer: "Comunicación",
+    index: "c"
+  },
+  {
+    carreer: "Diseño gráfico",
+    index: "d"
+  },
+  {
+    carreer: "Mercadotecnia",
+    index: "e"
+  },
+  {
+    carreer: "Gastronomía",
+    index: "f"
+  },
+  {
+    carreer: "Nutrición",
+    index: "g"
+  },
+  {
+    carreer: "Ingeniería",
+    index: "h"
+  },
+  {
+    carreer: "Derecho",
+    index: "i"
+  },
+]
